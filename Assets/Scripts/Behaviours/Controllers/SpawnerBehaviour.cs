bdcccee63d4a7b3bf1d5936d78b78623e2d61926
Ramon Saraiva <ramonsaraiva@gmail.com>
2016-05-01 22:29:37 -0300

    spawner marker

--- Assets/Scripts/Behaviours/Controllers/SpawnerBehaviour.cs
+++ Assets/Scripts/Behaviours/Controllers/SpawnerBehaviour.cs
@@ -4,61 +4,31 @@ using GameConstants;
 public class SpawnerBehaviour : MonoBehaviour
 {
     [SerializeField]
-    private GameObject zomBearPrefab;
+    private GameObject prefab;
     [SerializeField]
-    private GameObject zomBunnyPrefab;
+    private string name;
     [SerializeField]
-    private GameObject hellephantPrefab;
+    private float rate;
 
-    [SerializeField]
-    private Transform[] zomBunnySpawnLocations;
-    [SerializeField]
-    private Transform[] zomBearSpawnLocations;
-    [SerializeField]
-    private Transform[] hellephantSpawnLocations;
-
-    private float nextZomBunnySpawn;
-    private float nextZomBearSpawn;
-    private float nextHellephantSpawn;
+    private float nextSpawn;
 
 	private void Start()
     {
-        nextZomBunnySpawn = Time.time + LevelConstants.ZomBunnySpawnRate;
-        nextZomBearSpawn = Time.time + LevelConstants.ZomBearSpawnRate;
-        nextHellephantSpawn = Time.time + LevelConstants.HellephantSpawnRate;
+        nextSpawn = Time.time + rate;
 	}
 	
 	private void Update()
     {
-        SpawnZomBunny();
-        SpawnZomBear();
-        SpawnHellephant();
+        Spawn();
 	}
 
-    private void SpawnZomBunny()
+    private void Spawn()
     {
-        if (Time.time > nextZomBunnySpawn)
+        print("spawning");
+        if (Time.time > nextSpawn)
         {
-            nextZomBunnySpawn = Time.time + LevelConstants.ZomBunnySpawnRate;
-            GameObject zomBunny = PhotonNetwork.Instantiate("ZomBunny", zomBunnySpawnLocations[Random.Range(0, zomBunnySpawnLocations.Length)].position, Quaternion.identity, 0) as GameObject;
-        }
-    }
-
-    private void SpawnZomBear()
-    {
-        if (Time.time > nextZomBearSpawn)
-        {
-            nextZomBearSpawn = Time.time + LevelConstants.ZomBearSpawnRate;
-            GameObject zomBear = PhotonNetwork.Instantiate("ZomBear", zomBearSpawnLocations[Random.Range(0, zomBearSpawnLocations.Length)].position, Quaternion.identity, 0) as GameObject;
-        }
-    }
-
-    private void SpawnHellephant()
-    {
-        if (Time.time > nextHellephantSpawn)
-        {
-            nextHellephantSpawn = Time.time + LevelConstants.HellephantSpawnRate;
-            GameObject hellephant = PhotonNetwork.Instantiate("Hellephant", hellephantSpawnLocations[Random.Range(0, hellephantSpawnLocations.Length)].position, Quaternion.identity, 0) as GameObject;
+            nextSpawn = Time.time + rate;
+            GameObject enemy = PhotonNetwork.Instantiate(name, transform.position, Quaternion.identity, 0) as GameObject;
         }
     }
 }
---
+++ Assets/Scripts/Handlers/Buttons/SpawnEventHandler.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SpawnEventHandler : MonoBehaviour
+{
+    [SerializeField]
+    private Camera mainCamera;
+
+    [SerializeField]
+    private Transform environment;
+
+    [SerializeField]
+    private Transform groundOffset;
+
+    [SerializeField]
+    private List<GameObject> spawns;
+
+    private GameObject current;
+    private int current_index;
+
+    void Start()
+    {
+        current_index = 0;
+        ChangeSpawn();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits;
+
+            hits = Physics.RaycastAll(ray);
+
+            foreach (var hit in hits)
+            {
+                Transform objectHit = hit.transform;
+                if (objectHit.tag == "ChangeSpawn")
+                {
+                    ChangeSpawn();
+                }
+
+                if (objectHit.tag == "CreateSpawn")
+                {
+                    InstantiateSpawn();
+                }
+            }
+        }
+    }
+
+    private void ChangeSpawn()
+    {
+        Destroy(current);
+        GameObject spawn = spawns[current_index++ % spawns.Count];
+        Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y + spawn.transform.localScale.y / 2 + 14f, transform.position.z);
+        current = Instantiate(spawn, spawnPosition, transform.rotation) as GameObject;
+        current.transform.parent = transform;
+        DisablePrefabArenaBehaviours();
+    }
+
+    private void InstantiateSpawn()
+    {
+        Vector3 spawnPosition = new Vector3(current.transform.position.x, environment.position.y, current.transform.position.z);
+        GameObject spawn = PhotonNetwork.Instantiate(spawns[(current_index - 1) % spawns.Count].name, spawnPosition, transform.rotation, 0) as GameObject;
+    }
+
+    private void DisablePrefabArenaBehaviours()
+    {
+        current.GetComponent<SpawnerBehaviour>().enabled = false;
+    }
+}
